f89ef76f8266175360e3d410b50a65039f3c74dd
Mayer Martin Balázs <mayer.martin.balazs@gmail.com>
2025-07-16 17:21:14 +0200

    It's freeRTOS now

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
-#define LED_PIN 13
 
-// put function declarations here:
-int myFunction(int, int);
+TaskHandle_t xLedTaskHandle = NULL;
+
+void vLedTask(void *pvParameters) {
+    const TickType_t xDelay5000ms = pdMS_TO_TICKS(5000);
+    
+    for(;;) {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // Toggle LED
+        vTaskDelay(xDelay5000ms); // Várakozás 5000ms
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
+    // Serial inicializálása (opcionális)
+    Serial.begin(115200);
+    
+    // LED pin kimenetre állítása
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    
+    // LED task létrehozása
+    xTaskCreate(
+        vLedTask,               // Task függvény
+        "LED Blink",            // Task név
+        configMINIMAL_STACK_SIZE + 50, // Stack méret
+        NULL,                   // Paraméterek
+        1,                      // Prioritás
+        &xLedTaskHandle         // Task handle
+    );
+    
+    // A scheduler automatikusan indul a setup() lefutása után.
+    // A vTaskStartScheduler() hívása itt szükségtelen.
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); // turn the LED on
-  delay(1000);                 // wait for a second
-  digitalWrite(LED_PIN, LOW);  // turn the LED off
-  delay(1000);                 // wait for a second
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
+  // Ez a függvény üresen marad, mivel az ütemező futtatja a taszkokat.
+  // Alacsony prioritású idle task-ként funkcionálhat.
 }
